f8806ece6e186fa8c3f54bcaad7cb6c123891e1b
Titrat Cristina <titratcristina@gmail.com>
2020-01-04 16:47:53 +0200

    Update lab3_proceduri_suma-numere.asm

--- ASC/Laborator/Laborator-3/lab3_proceduri_suma-numere.asm
+++ ASC/Laborator/Laborator-3/lab3_proceduri_suma-numere.asm
@@ -1,3 +1,4 @@
+### in mars programul nu merge daca procedura este inainte de main :) ###
 # Calculati suma a doua numere prin intermediul procedurilor
 
 .data
@@ -29,28 +30,29 @@ main:
 	li $v0, 10
 	syscall
 	
-	suma:
-		subu $sp, $sp, 4
-		sw $fp, 0($sp)
+suma:
+	subu $sp, $sp, 4
+	sw $fp, 0($sp)
+	
+	addi $fp, $sp, 4
+	sw $s0, 0($sp)
+	
+	subu $sp, $sp, 4
+	sw $sp, 0($sp)
 		
-		addi $fp, $sp, 4
-		sw $s0, 0($sp)
+	lw $s0, 0($fp)
+	lw $s1, 4($fp)
+	
+	add $v0, $s0, $s1
 		
-		subu $sp, $sp, 4
-		sw $sp, 0($sp)
+	lw $s1, -12($fp)
+	lw $s0, -8($fp)
+	lw $fp, -4($fp)
+	
+	addi $sp, $sp, 12
 		
-		lw $s0, 0($fp)
-		lw $s1, 4($fp)
-		
-		add $v0, $s0, $s1
-		
-		lw $s1, -12($fp)
-		lw $s0, -8($fp)
-		lw $fp, -4($fp)
-		
-		addi $sp, $sp, 12
-		
-		jr $ra
-		
+	jr $ra
+	
+
 	
 	
